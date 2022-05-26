5d945954e7becf5c0aba126d5eab8e9e98131f56
Prabh <prabh@Roops-MacBook-Pro.local>
2022-05-25 23:21:18 -0600

    Algo to return smallest positive integer that does not occur in array

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,6 +1,40 @@
 import UIKit
 
 /*
+ Write a function:
+
+ public func solution(_ A : inout [Int]) -> Int
+
+ that, given an array A of N integers, returns the smallest positive integer (greater than 0) that does not occur in A.
+
+ For example, given A = [1, 3, 6, 4, 1, 2], the function should return 5.
+
+ Given A = [1, 2, 3], the function should return 4.
+
+ Given A = [−1, −3], the function should return 1.
+
+ Write an efficient algorithm for the following assumptions:
+
+ N is an integer within the range [1..100,000];
+ each element of array A is an integer within the range [−1,000,000..1,000,000].
+ */
+
+public func solution(_ A : [Int]) -> Int {
+    let positive = A.filter { $0 > 0 }.sorted()
+    var x = 1
+    for val in positive {
+      // if we find a smaller number no need to continue, cause the array is sorted
+      if(x < val) {
+       return x
+      }
+       x = val + 1
+    }
+  return x
+}
+
+print(solution([2,5,6,8,9,10]))
+
+/*
  Please write a piece of code that returns a substring of a given string with a known index. We don’t want to have truncated words in the returned string, so if the index is pointing to a character inside a word, we want the whole word to be included in the result.
  
  example:
@@ -51,3 +85,4 @@ func subStringOfGivenString(index: Int, inputString: String) -> String? {
 }
 
 print(subStringOfGivenString(index: 20, inputString: "I believe that people are really good at heart") ?? "nil")
+
