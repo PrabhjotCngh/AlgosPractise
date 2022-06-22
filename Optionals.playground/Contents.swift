5c1b9cd2f77b511b3eb32c3121549f14551b58a1
Prabh <prabh@Roops-MacBook-Pro.local>
2022-06-21 22:38:41 -0600

    Added more ways to handle optionals in swift

--- Optionals.playground/Contents.swift
+++ Optionals.playground/Contents.swift
@@ -2,16 +2,16 @@ import UIKit
 
 //MARK: - Unwrapping techniques - if let and guard
 
-struct Student {
+struct PrimaryStudent {
     let firstName: String
     let lastName: String
     var middleName: String?
     var grade: String?
 }
 
-var student = Student(firstName: "John", lastName: "Doe")
-student.middleName = "Johnson"
-student.grade = "A"
+var primaryStudent = PrimaryStudent(firstName: "John", lastName: "Doe")
+primaryStudent.middleName = "Johnson"
+primaryStudent.grade = "A"
 
 /*
 if let middleName = student.middleName, let grade = student.grade {
@@ -19,11 +19,11 @@ if let middleName = student.middleName, let grade = student.grade {
     print(grade)
 } */
 
-if let _ = student.grade {
+if let _ = primaryStudent.grade {
     print("Student has been graded")
 }
 
-func displayStudent(student: Student) {
+func displayStudent(student: PrimaryStudent) {
     
     guard let middleName = student.middleName,
           let grade = student.grade else {
@@ -33,11 +33,11 @@ func displayStudent(student: Student) {
     print(middleName, grade)
 }
 
-displayStudent(student: student)
+displayStudent(student: primaryStudent)
 
-//MARK: - Variable Shadowing - Using same variable name as a global variable to unwrap the optional in if let statement is  called as variable shadowing
+//MARK: - Variable Shadowing - Using same variable name as a global variable to unwrap the optional in if let statement is called as variable shadowing
 
-struct Student: CustomStringConvertible {
+struct SecondaryStudent: CustomStringConvertible {
     
     var description: String {
         var studentDescription = "\(firstName) "
@@ -63,13 +63,13 @@ struct Student: CustomStringConvertible {
     let grade: String?
 }
 
-let student = Student(firstName: "John", lastName: "Doe", middleName: "Smith", grade: "A")
+let secondaryStudent = SecondaryStudent(firstName: "John", lastName: "Doe", middleName: "Smith", grade: "A")
 
-print(student)
+print(secondaryStudent)
 
 //MARK: - Return Optional Strings
 
-struct Student {
+struct TertiaryStudent {
     let firstName: String?
     let lastName: String?
     
@@ -84,16 +84,94 @@ struct Student {
     }
 }
 
-let student = Student(firstName: "John", lastName: "Doe")
+let tertiaryStudent = TertiaryStudent(firstName: "John", lastName: "Doe")
 
-func createGreetingMessage(student: Student) -> String {
+func createGreetingMessage(student: TertiaryStudent) -> String {
    let message = """
         Dear \(student.displayName ?? "Student"), Welcome to Swift University
     """
     return message
 }
 
-let message = createGreetingMessage(student: student)
+let message = createGreetingMessage(student: tertiaryStudent)
 print(message)
 
 //MARK: - Chaining Optionals
+
+struct Grade {
+    let gpa: Double?
+    let letter: String?
+}
+
+struct UniversityStudent {
+    let firstName: String
+    let lastName: String
+    let grade: Grade?
+}
+
+let universityStudent = UniversityStudent(firstName: "John", lastName: "Doe", grade: Grade(gpa: 3.2, letter: "B"))
+
+/*
+if let grade = student.grade {
+    if let gpa = grade.gpa {
+        //print(gpa)
+    }
+} */
+
+print(universityStudent.grade?.gpa ?? "N/A")
+
+//MARK: - Optionals and Booleans
+
+
+enum UserAgreement: RawRepresentable {
+    case accepted
+    case rejected
+    case notSet
+    
+    init(rawValue: Bool?) {
+        switch rawValue {
+            case true?: self = .accepted
+            case false?: self = .rejected
+            default: self = .notSet
+        }
+    }
+    
+    var rawValue: Bool? {
+        switch self {
+            case .accepted: return true
+            case .rejected: return false
+            case .notSet: return nil
+        }
+    }
+    
+}
+
+let userAgreement = UserAgreement(rawValue: true)
+
+switch userAgreement {
+    case .accepted:
+        print("accepted")
+    case .rejected:
+        print("rejected")
+    case .notSet:
+        print("notSet")
+}
+
+
+//MARK: - Force Unwrapping
+
+struct Student {
+    let firstName: String
+    let lastName: String
+    let grade: String?
+}
+
+let student = Student(firstName: "John", lastName: "Doe", grade: nil)
+
+//print(student.grade!)
+
+guard let url = URL(string: "aa bbb cc") else {
+    fatalError("URL is not defined!")
+}
+
+print(url)
